99bdcb54b878a2769edbb57ab149de336152c8f9
LLeo <lleoem@gmail.com>
2016-08-23 02:35:28 -0400

    added documentation

--- string_key/string_key.go
+++ string_key/string_key.go
@@ -1,3 +1,14 @@
+/*
+The "github.com/lleo/go-hamt/string_key package" is an implementation of the
+"github.com/lleo/go-hamt/hamt_key.Key" interface.
+
+Both of these packages are are in separate package namespaces to prevent circular
+references between "github.com/lleo/go-hamt" and "github.com/lleo/go-hamt/hamt32"
+and/or "github.com/lleo/go-hamt/hamt64".
+
+Additionally, "github.com/lleo/go-hamt/string_key" is used by the functional
+variation of the Hash Array Mapped Trie in "github.com/lleo/go-hamt-fuctional".
+*/
 package string_key
 
 import (
@@ -8,6 +19,9 @@ import (
 
 type StringKey string
 
+// Equals returns true iff the StringKey exactly matches the key passed it. The
+// hamt_key.Key passed as an argument MUST BE a StringKey or the method Equals()
+// automatically returns false.
 func (sk StringKey) Equals(key hamt_key.Key) bool {
 	var k, typeMatches = key.(StringKey)
 	if !typeMatches {
@@ -16,10 +30,16 @@ func (sk StringKey) Equals(key hamt_key.Key) bool {
 	return string(sk) == string(k)
 }
 
+// The Hash30() calculates the fnv1 32bit hash of the string (redered to its bytes).
+// Then we use the xor-fold technique described <a href="http://www.isthe.com/chongo/tech/comp/fnv/index.html#xor-fold">here</a>
+// to fold the top 2bits into the lower 30bits of the 32bit hash value.
 func (sk StringKey) Hash30() uint32 {
 	return hash30(sk.hash32())
 }
 
+// The Hash60() calculates the fnv1 64bit hash of the string (redered to its bytes).
+// Then we use the xor-fold technique described <a href="http://www.isthe.com/chongo/tech/comp/fnv/index.html#xor-fold">here</a>
+// to fold the top 4bits into the lower 60bits of the 64bit hash value.
 func (sk StringKey) Hash60() uint64 {
 	return hash60(sk.hash64())
 }
@@ -47,6 +67,7 @@ func hash60(h64 uint64) uint64 {
 	return (h64 >> 60) ^ (h64 & mask60)
 }
 
+// String() returns the StringKey as a basic string type.
 func (sk StringKey) String() string {
 	return string(sk)
 }
