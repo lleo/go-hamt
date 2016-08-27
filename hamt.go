5c450c3ace0262b9308d4c87b7c24a14db103e50
LLeo <lleoem@gmail.com>
2016-08-27 14:50:51 -0400

    lots of un-exporting of symbols; removed all tags

--- hamt.go
+++ hamt.go
@@ -1,3 +1,16 @@
+/*
+This is the unifying package between "github.com/lleo/go-hamt/hamt32" and
+"github.com/lleo/go-hamt/hamt64". You can `import hamt "github.com/lleo/go-hamt"'
+then instantiate either a hamt32 or hamt64 datastructure with the
+hamt.NewHamt32() or hamt.NewHamt64() functions. Both datastructures have the
+same exported API defined by the Hamt interface.
+
+You may implement your own Key type by implementeding the Key interface
+defined in "github.com/lleo/go-hamt/hamt_key" or you may used the example
+StringKey interface described in "github.com/lleo/go-hamt/string_key".
+
+A HAMT is a Hashed Array Mapped Trie datastructure. FIXME: explain HAMT
+*/
 package hamt
 
 import (
@@ -16,13 +29,6 @@ type Hamt interface {
 	String() string
 	LongString(indent string) string
 }
-
-//type Key interface {
-//	Equals(Key) bool
-//	Hash30() uint32
-//	Hash60() uint64
-//	String() string
-//}
 
 type keyVal struct {
 	key hamt_key.Key
--- hamt_key/hamt_key.go
+++ hamt_key/hamt_key.go
@@ -1,9 +1,9 @@
 /*
-The hamt_key package contains a single Key interface. The hamt_key package was
+Package hamt_key contains a single Key interface. The hamt_key package was
 created to prevent cicular depedencies betwee "github.com/lleo/go-hamt" and
 either "github.com/lleo/go-hamt/hamt32" or "github.com/lleo/go-hamt/hamt64".
 
-However the hamt_key pacakge is also used by the functional Hamt variation in
+However the hamt_key pacakge is also used by the functional HAMT variation in
 "github.com/lleo/go-hamt-functional".
 */
 package hamt_key
