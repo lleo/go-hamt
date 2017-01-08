b19b2ade9326ad3ee4655ef429020fb7387b5e06
LLeo <lleoem@gmail.com>
2017-01-08 00:03:29 -0500

    shell scripts to run benchmarks

---
+++ hamt32/summary.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Map verus Hamt32 w/fullTables only"
+benchcmp map.b fullonly.b
+echo
+echo "fullTables only versus compressedTables only"
+benchcmp fullonly.b componly.b
+echo
+echo "fullTables only versus hybrid strategy (default)"
+benchcmp fullonly.b hybrid.b
+echo
+echo "compressedTables only versus hybrid strategy (default)"
+benchcmp componly.b hybrid.b
