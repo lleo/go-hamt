021c2873b2fa813615eb7dd403c0011ac183cb6d
LLeo <lleoem@gmail.com>
2017-04-02 05:43:01 -0400

    added a bunch of methods on key interface

--- key/key.go
+++ key/key.go
@@ -36,13 +36,20 @@ func (kv KeyVal) String() string {
 // in either the github.com/lleo/go-hamt or github.com/lleo/go-hamt-functional
 // packages.
 type Key interface {
-	String() string // ISA/statisfies fmt.Stringer interface
-	String30() string
-	String60() string
-	Equals(Key) bool
 	Hash30() uint32
 	Hash60() uint64
+	Equals(Key) bool
+	String() string
+	String30() string
+	String60() string
+	Index30(depth uint) uint
+	Index60(depth uint) uint
+	HashPathString30(depth uint) string
+	HashPathString60(depth uint) string
 }
+
+type HashVal30 uint32
+type HashVal60 uint64
 
 // Base struct is intended to be the base struct of all structs that satisfy
 // Key interface. It caches the calculated hash30 and hash60 generated when
@@ -52,99 +59,112 @@ type Base struct {
 	hash60 uint64
 }
 
-// INTERESTING IDEA JUST NOT YET...
-//
-// type Hash30 uint32
-//
-// func (h30 Hash30) String() {
-// 	return hash30String(uint32(h30))
-// }
-//
-// func (h30 Hash30) index(depth uint) {
-// 	return index(uint32(h30), depth)
-// }
-//
-// type Hash60 uint64
-//
-// func (h Hash60) String() {
-// 	return hash60String(uint64(h))
-// }
-//
-// func (h60 Hash60) index(depth uint) {
-// 	return index(uint64(h60), depth)
-// }
+const BitsPerLevel30 uint = 5
+const BitsPerLevel60 uint = 6
 
-//
-// 30bit hash value and string generator functions
-// Hash values are generated when Initialize([]byte) is called.
-// Hash strings are generated as needed.
-//
+const MaxDepth30 uint = 5
+const MaxDepth60 uint = 9
 
-const nBits30 uint = 5
+// Index30() will return a 5bit (aka BitsPerLevel30) value 'depth' number
+// of 5bits from the beginning of the uint32 kb.base30 value.
+func (kb Base) Index30(depth uint) uint {
+	var idxMask = indexMask30(depth)
+	var idx = uint(kb.hash30&idxMask) >> (depth * BitsPerLevel30)
+	return idx
+}
 
 func indexMask30(depth uint) uint32 {
-	return uint32(uint8(1<<nBits30)-1) << (depth * nBits30)
+	return uint32((1<<BitsPerLevel30)-1) << (depth * BitsPerLevel30)
 }
 
-func index30(h30 uint32, depth uint) uint {
-	var idxMask = indexMask30(depth)
-	var idx = uint((h30 & idxMask) >> (depth * nBits30))
+// Index60() will return a 6bit (aka BitsPerLevel60) value 'depth' number
+// of 6bits from the beginning of the uint64 h60 value.
+func (kb Base) Index60(depth uint) uint {
+	var idxMask = indexMask60(depth)
+	var idx = uint(kb.hash60&idxMask) >> (depth * BitsPerLevel60)
 	return idx
 }
-
-func hashPathString30(hashPath uint32, depth uint) string {
-	if depth == 0 {
-		return "/"
-	}
-	var strs = make([]string, depth+1)
-
-	for d := uint(0); d <= depth; d++ {
-		var idx = index30(hashPath, d)
-		strs[d] = fmt.Sprintf("%02d", idx)
-	}
-
-	return "/" + strings.Join(strs, "/")
-}
-
-func hash30String(h30 uint32) string {
-	return hashPathString30(h30, 5)
-}
-
-//
-// 60bit hash value and string generator functions.
-// Hash values are generated when Initialize([]byte) is called.
-// Hash strings are generated as needed.
-//
-
-const nBits60 uint = 6
 
 func indexMask60(depth uint) uint64 {
-	return uint64(uint64(1<<nBits60)-1) << (depth * nBits60)
+	return uint64((1<<BitsPerLevel60)-1) << (depth * BitsPerLevel60)
 }
 
-func index60(h60 uint64, depth uint) uint {
-	var idxMask = indexMask60(depth)
-	var idx = uint((h60 & idxMask) >> (depth * nBits60))
-	return idx
-}
+// HashPathString30() returns a string representation of the index path of
+// a hash30 30 bit value; that is depth number of zero padded numbers between
+// "00" and "63" separated by "/" characters.
+// Warning: It will panic() if depth > MaxDepth30.
+// Example: "/00/24/46/17" for depth=4 of a hash30 value represented
+//       by "/00/24/46/17/34/08".
+func (kb Base) HashPathString30(depth uint) string {
+	if depth > MaxDepth30 {
+		panic(fmt.Sprintf("HashPathString30: depth,%d > MaxDepth30,%d\n", depth, MaxDepth30))
+	}
 
-func hashPathString60(hashPath uint64, depth uint) string {
 	if depth == 0 {
 		return "/"
 	}
-	var strs = make([]string, depth+1)
 
-	for d := uint(0); d <= depth; d++ {
-		var idx = index60(hashPath, d)
+	var strs = make([]string, depth)
+
+	for d := uint(0); d < depth; d++ {
+		var idx = kb.Index30(d)
 		strs[d] = fmt.Sprintf("%02d", idx)
 	}
 
 	return "/" + strings.Join(strs, "/")
 }
 
-func hash60String(h60 uint64) string {
-	return hashPathString60(h60, 9)
+// HashPathString60() returns a string representation of the index path of
+// a hash60 60 bit value; that is depth number of zero padded numbers between
+// "00" and "63" separated by "/" characters.
+// Warning: It will panic() if depth > MaxDepth60.
+// Example: "/00/24/46/17/34/08/54" for depth=7 of a hash60 value represented
+//       by "/00/24/46/17/34/08/54/28/59/51".
+func (kb Base) HashPathString60(depth uint) string {
+	if depth > MaxDepth60 {
+		panic(fmt.Sprintf("PathString60: depth,%d > MaxDepth60,%d\n", depth, MaxDepth60))
+	}
+
+	if depth == 0 {
+		return "/"
+	}
+
+	var strs = make([]string, depth)
+
+	for d := uint(0); d < depth; d++ {
+		var idx = kb.Index60(d)
+		strs[d] = fmt.Sprintf("%02d", idx)
+	}
+
+	return "/" + strings.Join(strs, "/")
 }
+
+// String() provides a human readable form of the Base's hash30 and hash60
+// values. Both in raw integer and slash '/' separated integers of the 5 and
+// 6 bit path values; for hash30 and hash60 respectively.
+func (kb Base) String() string {
+	return fmt.Sprintf("Base{%s, %s}", kb.String30(), kb.String60())
+}
+
+// String30() returns a string representation of the kb.hash30 value. This
+// is MaxDepth30+1(6) two digit numbers (zero padded) between "00" and "31"
+// seperated by '/' characters.
+// Example: "/08/14/28/20/00/31"
+func (kb Base) String30() string {
+	return kb.HashPathString30(MaxDepth30)
+}
+
+// String60() returns a string representation of the kb.hash60 value. This
+// is MaxDepth60+1(10) two digit numbers (zero padded) between "00" and "63"
+// seperated by '/' characters.
+// Example: "/08/14/28/20/00/31/56/01/24/63"
+func (kb Base) String60() string {
+	return kb.HashPathString60(MaxDepth60)
+}
+
+//
+// Calculate Hash of byte slice functions
+//
 
 const mask30 = uint32(1<<30) - 1
 
@@ -176,26 +196,12 @@ func hash64(bs []byte) uint64 {
 	return h.Sum64()
 }
 
-// String provides a human readable form of the Base's hash30 and hash60
-// values. Both in raw integer and slash '/' separated integers of the 5 and
-// 6 bit path values; for hash30 and hash60 respectively.
-func (kb Base) String() string {
-	return fmt.Sprintf("Base{%s, %s}", hash30String(kb.hash30), hash60String(kb.hash60))
-}
-
-func (kb Base) String30() string {
-	return hash30String(kb.hash30)
-}
-
-func (kb Base) String60() string {
-	return hash60String(kb.hash60)
-}
-
-// Initialize the Base part of any struct that has the Base struct embedded.
-// will be called by the New() function of any descendant class.
+// Initialize the calculates the Base part of any struct that has the Base
+// struct embedded. It will be called by the New() function of any descendant
+// class.
 //     k.Initialize(bs)
 // where k is interpreted as a *Base and bs is a unique []byte to calculate
-// the 30bit hash from.
+// the 30bit and 60bit hash values from.
 func (kb *Base) Initialize(bs []byte) {
 	kb.hash30 = fold30(hash32(bs))
 	kb.hash60 = fold60(hash64(bs))
