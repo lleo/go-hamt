1904984e9190926d8af9f6f968dee473999bda4c
LLeo <lleoem@gmail.com>
2017-05-09 16:15:13 -0400

    added more text conversions

--- convert32to64.sh
+++ convert32to64.sh
@@ -2,10 +2,23 @@
 
 for f in "$@"; do
 	echo $f
+
 	echo "perl -pi -e 's/32/64/g' $f"
 	perl -pi -e 's/32/64/g' $f
+
 	echo "perl -pi -e 's/30/60/g' $f"
 	perl -pi -e 's/30/60/g' $f
-	#echo "perl -pi -e 's/six/ten/g'"
-done
 
+	echo "perl -pi -e 's/\bsix/ten/g'"
+	perl -pi -e 's/\bsix/ten/g' $f
+
+	echo "perl -pi -e 's/\b5bit/6bit/g'"
+	perl -pi -e 's/\b5bit/6bit/g' $f
+
+	echo "perl -pi -e 's/\b6\b/9/g'"
+	perl -pi -e 's/\b6\b/9/g' $f
+
+	echo "perl -pi -e 's/\b5\b/6/g'"
+	perl -pi -e 's/\b5\b/6/g' $f
+
+done
