3dd9727431335b0ef62d7351a51b5918d082f8c0
LLeo <lleoem@gmail.com>
2017-05-17 06:20:14 -0400

    added one conversion and updated another

--- convert32to64.sh
+++ convert32to64.sh
@@ -6,6 +6,9 @@ for f in "$@"; do
 	echo "perl -pi -e 's/32/64/g' $f"
 	perl -pi -e 's/32/64/g' $f
 
+	echo "perl -pi -e 's/31/63/g' $f"
+	perl -pi -e 's/31/63/g' $f
+
 	echo "perl -pi -e 's/30/60/g' $f"
 	perl -pi -e 's/30/60/g' $f
 
@@ -15,8 +18,8 @@ for f in "$@"; do
 	echo "perl -pi -e 's/\b5bit/6bit/g'"
 	perl -pi -e 's/\b5bit/6bit/g' $f
 
-	echo "perl -pi -e 's/\b6\b/9/g'"
-	perl -pi -e 's/\b6\b/9/g' $f
+	echo "perl -pi -e 's/\b6\b/10/g'"
+	perl -pi -e 's/\b6\b/10/g' $f
 
 	echo "perl -pi -e 's/\b5\b/6/g'"
 	perl -pi -e 's/\b5\b/6/g' $f
