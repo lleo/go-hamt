68fef9b373b2e4e6e4b72daea91cd713062453ac
LLeo <lleoem@gmail.com>
2017-07-05 20:10:35 -0400

    removed mean and stddev from stored data structure & added fixup logic

--- hamt-mean-stddev.pl
+++ hamt-mean-stddev.pl
@@ -4,6 +4,8 @@ use v5.12;
 use Statistics::Basic qw(:all);
 use Data::Dumper;
 use Getopt::Long;
+use File::Basename qw(basename dirname);
+use File::Spec::Functions qw(catfile);
 
 $| = 1; #turn on autoflush
 
@@ -36,49 +38,49 @@ $help && UsageAndExit(0);
 my $s;
 if ($input_fn eq '-') {
 	$s = {'32'=>{'transient'=>
-	             {'full'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'comp'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'hybr'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}}
+	             {'fixed' =>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'sparse'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'hybrid'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}}
 	             },
 	             'functional'=>
-	             {'full'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'comp'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'hybr'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}}
+	             {'fixed' =>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'sparse'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'hybrid'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}}
 	             }
 	            },
 	      '64'=>{'transient'=>
-	             {'full'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'comp'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'hybr'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}}
+	             {'fixed' =>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'sparse'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'hybrid'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}}
 	             },
 	             'functional'=>
-	             {'full'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'comp'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}},
-	              'hybr'=>{'get'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'put'=>{'data'=>[],'mean'=>0,'stddev'=>0},
-	                       'del'=>{'data'=>[],'mean'=>0,'stddev'=>0}}
+	             {'fixed' =>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'sparse'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}},
+	              'hybrid'=>{'get'=>{'data'=>[]},
+	                         'put'=>{'data'=>[]},
+	                         'del'=>{'data'=>[]}}
 	             }
 	            }
 	     };
@@ -100,77 +102,18 @@ if ($input_fn eq '-') {
 			next LINE;
 		};
 		# Set the current $ttype
-		$l =~ m/TableOption=Full/ && do {
-			$ttype = 'full';
-			next LINE;
-		};
-		$l =~ m/TableOption=Comp/ && do {
-			$ttype = 'comp';
-			next LINE;
-		};
-		$l =~ m/TableOption=Hybr/ && do {
-			$ttype = 'hybr';
+		#$l =~ m/TableOption=(Fixed|Sparse|Hybrid)/ && do {
+		$l =~ m/TableOption=(\p{Alpha}+)T/ && do {
+			$ttype = lc($1); # 'fixed' | 'sparse' | 'hybrid'
+			#if (length($ttype) < 6) {
+			#	$ttype .= ' 'x(6-length($ttype))
+			#}
 			next LINE;
 		};
 		# record the benchmark result
-		$l =~ m/^BenchmarkHamt32Get-8/ && do {
-			my $bit = '32';
-			my $op = 'get';
-			my @fields = split(' ', $l);
-			push @{$s->{$bit}{$mode}{$ttype}{$op}{'data'}}, $fields[2];
-			my $n = scalar(@{$s->{$bit}{$mode}{$ttype}{$op}{'data'}});
-			say STDERR "#$n: ", join(" ", $bit, $mode, $ttype, $op, $fields[2]);
-			#local $Data::Dumper::Indent = 0;
-			#say STDERR Dumper( $s->{$bit}{$mode}{$ttype}{$op}{'data'} );
-			next LINE;
-		};
-		$l =~ m/^BenchmarkHamt32Put-8/ && do {
-			my $bit = '32';
-			my $op = 'put';
-			my @fields = split(' ', $l);
-			push @{$s->{$bit}{$mode}{$ttype}{$op}{'data'}}, $fields[2];
-			my $n = scalar(@{$s->{$bit}{$mode}{$ttype}{$op}{'data'}});
-			say STDERR "#$n: ", join(" ", $bit, $mode, $ttype, $op, $fields[2]);
-			#local $Data::Dumper::Indent = 0;
-			#say STDERR Dumper( $s->{$bit}{$mode}{$ttype}{$op}{'data'} );
-			next LINE;
-		};
-		$l =~ m/^BenchmarkHamt32Del-8/ && do {
-			my $bit = '32';
-			my $op = 'del';
-			my @fields = split(' ', $l);
-			push @{$s->{$bit}{$mode}{$ttype}{$op}{'data'}}, $fields[2];
-			my $n = scalar(@{$s->{$bit}{$mode}{$ttype}{$op}{'data'}});
-			say STDERR "#$n: ", join(" ", $bit, $mode, $ttype, $op, $fields[2]);
-			#local $Data::Dumper::Indent = 0;
-			#say STDERR Dumper( $s->{$bit}{$mode}{$ttype}{$op}{'data'} );
-			next LINE;
-		};
-		$l =~ m/^BenchmarkHamt64Get-8/ && do {
-			my $bit = '64';
-			my $op = 'get';
-			my @fields = split(' ', $l);
-			push @{$s->{$bit}{$mode}{$ttype}{$op}{'data'}}, $fields[2];
-			my $n = scalar(@{$s->{$bit}{$mode}{$ttype}{$op}{'data'}});
-			say STDERR "#$n: ", join(" ", $bit, $mode, $ttype, $op, $fields[2]);
-			#local $Data::Dumper::Indent = 0;
-			#say STDERR Dumper( $s->{$bit}{$mode}{$ttype}{$op}{'data'} );
-			next LINE;
-		};
-		$l =~ m/^BenchmarkHamt64Put-8/ && do {
-			my $bit = '64';
-			my $op = 'put';
-			my @fields = split(' ', $l);
-			push @{$s->{$bit}{$mode}{$ttype}{$op}{'data'}}, $fields[2];
-			my $n = scalar(@{$s->{$bit}{$mode}{$ttype}{$op}{'data'}});
-			say STDERR "#$n: ", join(" ", $bit, $mode, $ttype, $op, $fields[2]);
-			#local $Data::Dumper::Indent = 0;
-			#say STDERR Dumper( $s->{$bit}{$mode}{$ttype}{$op}{'data'} );
-			next LINE;
-		};
-		$l =~ m/^BenchmarkHamt64Del-8/ && do {
-			my $bit = '64';
-			my $op = 'del';
+		$l =~ m/^BenchmarkHamt(32|64)(Get|Put|Del)-/ && do {
+			my $bit = $1; # '32' | '64'
+			my $op = lc($2); # 'get' | 'put' | 'del'
 			my @fields = split(' ', $l);
 			push @{$s->{$bit}{$mode}{$ttype}{$op}{'data'}}, $fields[2];
 			my $n = scalar(@{$s->{$bit}{$mode}{$ttype}{$op}{'data'}});
@@ -188,29 +131,74 @@ if ($input_fn eq '-') {
 		$fh->close();
 	}
 } else {
-	#say "\$s = do $input_fn";
 	$s = do $input_fn;
+
+	# fixup old $ttype names
+	my ($need_write);
+	for my $bit (keys %$s) {
+		for my $mode (keys %{$s->{$bit}}) {
+			if (ref($s->{$bit}{$mode}{'fixed '}) eq 'HASH') {
+				$s->{$bit}{$mode}{'fixed'} = delete $s->{$bit}{$mode}{'fixed '};
+				$need_write = "true";
+			}
+			if (ref($s->{$bit}{$mode}{'spars'}) eq 'HASH') {
+				$s->{$bit}{$mode}{'sparse'} = delete $s->{$bit}{$mode}{'spars'};
+				$need_write = "true";
+			}
+			if (ref($s->{$bit}{$mode}{'hybri'}) eq 'HASH') {
+				$s->{$bit}{$mode}{'hybrid'} = delete $s->{$bit}{$mode}{'hybri'};
+				$need_write = "true";
+			}
+		}
+	}
+	for my $bit (keys %$s) {
+		for my $mode (keys %{$s->{$bit}}) {
+			for my $ttype (keys %{$s->{$bit}{$mode}}) {
+				for my $op (keys %{$s->{$bit}{$mode}{$ttype}}) {
+					if (ref($s->{$bit}{$mode}{$ttype}{$op}{'mean'})) {
+						delete $s->{$bit}{$mode}{$ttype}{$op}{'mean'};
+						$need_write = "true"
+					}
+					if (ref $s->{$bit}{$mode}{$ttype}{$op}{'stdev'}) {
+						delete $s->{$bit}{$mode}{$ttype}{$op}{'stdev'};
+						$need_write = "true"
+					}
+				}
+			}
+		}
+	}
+
+	if ($need_write) {
+		my $fixedup_input_fn = catfile(dirname($input_fn),
+		                               "fixedup-" . basename($input_fn));
+		open(my $fh, ">", $fixedup_input_fn)
+		  or die "Can't open > $fixedup_input_fn: $!";
+		$fh->print(Dumper($s));
+		$fh->close();
+		say "WROTE $fixedup_input_fn";
+	}
 }
 
 
-# Calculate mean & dev for each data vector
+# Calculate and Display mean & stddev for each data vector
 say "### REPORT ###";
-#for my $bit (keys %$s) {
-for my $bit ('32', '64') {
-	#for my $mode (keys %{$s->{$bit}}) {
-	for my $mode ('functional', 'transient') {
-		#for my $ttype (keys %{$s->{$bit}{$mode}}) {
-		for my $ttype ('full', 'comp', 'hybr') {
-			#for my $op (keys %{$s->{$bit}{$mode}{$ttype}}) {
+#for my $bit ('32', '64') {
+for my $bit (sort keys %$s) {
+	for my $mode (sort keys %{$s->{$bit}}) {
+		#for my $ttype (sort keys %{$s->{$bit}{$mode}}) {
+		for my $ttype ('fixed', 'sparse', 'hybrid') {
+			#for my $op (sort keys %{$s->{$bit}{$mode}{$ttype}}) {
 			for my $op ('get', 'put', 'del') {
 				my $entry = $s->{$bit}{$mode}{$ttype}{$op};
 				@{$entry->{'data'}} == 0 && next;
 				my $v = vector(@{$entry->{'data'}});
-				my $m = $entry->{'mean'} = mean($v);
-				my $d = $entry->{'stddev'} = stddev($v);
+				my $m = mean($v);
+				my $d = stddev($v);
 				my $pct = ($d / $m) * 100;
-				my $pct_s = sprintf("%%%.1f", $pct);
-				say "$bit/$mode/$ttype/$op => $m +/- $pct_s ($d) ns";
+				my $pct_s = sprintf("%%%-4.1f", $pct);
+				#say "$bit $mode $ttype $op => $m +/- $pct_s ($d) ns";
+				printf("%s %- 10s %- 6s %s => % 8.1f +/- %%%5.2f (%8.2f) ns\n",
+				       $bit, $mode, $ttype, $op, $m, $pct, $d);
 			}
 		}
 	}
