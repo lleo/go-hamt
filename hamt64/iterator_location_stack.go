60e19dd4eb70b4f45e29cafb0396c7c8f5c8ce07
LLeo <lleoem@gmail.com>
2017-08-08 03:58:35 -0400

    added iterLocStack struct to remember iter locations during traversal

---
+++ hamt64/iterator_location_stack.go
@@ -0,0 +1,44 @@
+package hamt64
+
+import (
+	"fmt"
+	"strings"
+)
+
+type iterLocation struct {
+	tab tableI
+	idx uint
+}
+
+type iterLocStack []iterLocation
+
+func (is *iterLocStack) len() int {
+	return len(*is)
+}
+
+func (is *iterLocStack) push(tab tableI, idx uint) *iterLocStack {
+	_ = assertOn && assert(tab != nil, "tab in tableStat is nil")
+	*is = append(*is, iterLocation{tab, idx})
+	return is
+}
+
+func (is *iterLocStack) pop() (tableI, uint) {
+	var last = is.len() - 1
+	var tab = (*is)[last].tab
+	var idx = (*is)[last].idx
+
+	*is = (*is)[:last]
+
+	return tab, idx
+}
+
+func (is *iterLocStack) String() string {
+	var strs = make([]string, is.len())
+
+	for i := 0; i < is.len(); i++ {
+		var ise = (*is)[i]
+		strs[i] = fmt.Sprintf("%d: tableI=%s; idx=%d;", i, ise.tab, ise.idx)
+	}
+
+	return strings.Join(strs, "\n") + "\n"
+}
